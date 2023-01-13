85d1663d5ca4aef477c42b27f2909d2991947944
Hasan Mubarok <hasanmubarok32@gmail.com>
2023-01-13 09:00:40 +0700

    init setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div id="search-bar">
+        <div className="title">
+          <h1>Github Search</h1>
+        </div>
+        <input type="text" placeholder="Enter Username" name="username" />
+        <button>Search</button>
+      </div>
     </div>
   );
 }
